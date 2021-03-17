8cc8f99506d1ef748bac45b1107ab1b2832a46c4
hsinhoyeh <yhh92u@gmail.com>
2021-03-17 17:42:18 +0800

    sdk: bump version

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="tintin-sdk",
-    version="0.0.1",
+    version="0.0.2",
     description="A Python SDK for Tintin",
     long_description=README,
     long_description_content_type="text/markdown",
